b2dec0b67f065d1e09f1e111d42c8f86eae60ab9
sayedsayedjamil@gmail.com <sayedsayedjamil@gmail.com>
2022-05-15 15:51:59 -0400

    New Home Work

---
+++ src/test/java/step/definitions/HomeStepDef.java
@@ -0,0 +1,45 @@
+package step.definitions;
+
+import core.Base;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class HomeStepDef extends Base {
+
+	@Given("user is on Retail home Page")
+	public void userIsOnRuser_is_on_retail_home_pageetailHomePage() {
+		
+	}
+
+	@When("user click on MyAccount")
+	public void user_click_on_my_account() {
+		// Write code here that turns the phrase above into concrete actions
+
+	}
+
+	@When("user click on Login option")
+	public void user_click_on_login_option() {
+		// Write code here that turns the phrase above into concrete actions
+
+	}
+
+	@When("user enter userName {string}  and password {string}")
+	public void user_enter_user_name_and_password(String string, String string2) {
+		// Write code here that turns the phrase above into concrete actions
+
+	}
+
+	@When("user click on login button")
+	public void user_click_on_login_button() {
+		// Write code here that turns the phrase above into concrete actions
+
+	}
+
+	@Then("user should be logged into myAccount Page")
+	public void user_should_be_logged_into_my_account_page() {
+		// Write code here that turns the phrase above into concrete actions
+
+	}
+
+}
